34bc44741ff93e6db2693c706b3a42df2bbc4d91
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-03-26 11:16:52 +0700

    Edit Product service methods to call notify after create/delete.

--- src/service/product.rs
+++ src/service/product.rs
@@ -13,6 +13,8 @@ impl ProductService {
         product.product_type = product.product_type.to_uppercase();
         let product_result: Product = ProductRepository::add(product);
 
+        NotificationService.notify(&product_result.product_type, "CREATED",
+                                   product_result.clone());
         return Ok(product_result);
     }
 
@@ -41,6 +43,7 @@ impl ProductService {
         }
         let product: Product = product_opt.unwrap();
 
+        NotificationService.notify(&product.product_type, "DELETED", product.clone());
         return Ok(Json::from(product));
     }
 
